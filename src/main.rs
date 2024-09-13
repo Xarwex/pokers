ebb2dbe7269e744f2ed8d6c1c9003b1d8ac48325
Xarwex <60325364+Xarwex@users.noreply.github.com>
2024-09-13 23:12:53 +0100

    v1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use clap::{Args, Parser, Subcommand};
+use rascii_art::RenderOptions;
+use tempdir::TempDir;
+use tokio::{fs::File, io::AsyncWriteExt};
 
 #[derive(Parser)]
 struct Cli {
@@ -27,11 +30,39 @@ async fn main() {
     let cli = Cli::parse();
     let client = rustemon::client::RustemonClient::default();
 
-    let pokemon = match &cli.command {
-        Commands::Name(name) => rustemon::pokemon::pokemon::get_by_name(&name.name, &client).await,
-        Commands::Id(id) => rustemon::pokemon::pokemon::get_by_id(id.id, &client).await,
-    }
-    .unwrap();
+    let mut print_name = false;
 
-    println!("{}", pokemon.name)
+    let Ok(pokemon) = (match &cli.command {
+        Commands::Name(name) => rustemon::pokemon::pokemon::get_by_name(&name.name, &client).await,
+        Commands::Id(id) => {
+            print_name = true;
+            rustemon::pokemon::pokemon::get_by_id(id.id, &client).await
+        }
+    }) else {
+        println!("Pokemon not found :(");
+        return;
+    };
+
+    if let Some(url) = pokemon.sprites.front_default {
+        //get the sprite
+        let sprite = reqwest::get(url).await.unwrap().bytes().await.unwrap();
+        let tmp_dir = TempDir::new("pokers").unwrap();
+        let file_path = tmp_dir.path().join("sprite.png");
+        let mut file = File::create_new(file_path.clone()).await.unwrap();
+        file.write_all(sprite.as_ref()).await.unwrap();
+        let mut buf = String::new();
+        rascii_art::render_to(
+            file_path.to_str().unwrap(),
+            &mut buf,
+            &RenderOptions::new().width(100).colored(true),
+        )
+        .unwrap();
+        println!("{}", buf);
+    } else {
+        println!("No sprite found for {}", pokemon.name)
+    }
+
+    if print_name {
+        println!("{}!", pokemon.name.to_uppercase())
+    }
 }
