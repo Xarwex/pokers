86f9e0bf0f384938be74f6c3186bd8c234d7f6b7
Aleksander Kleinszmidt <AKleinszmidt@veritionfund.com>
2026-02-05 15:03:08 +0000

    gitignore + package

--- flake.nix
+++ flake.nix
@@ -21,8 +21,31 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+
+        manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = pkgs.rust-bin.stable.latest.default;
+          rustc = pkgs.rust-bin.stable.latest.default;
+        };
+
+        pokers = rustPlatform.buildRustPackage {
+          pname = manifest.name;
+          version = manifest.version;
+          src = pkgs.lib.cleanSource ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          buildInputs = [ pkgs.openssl ];
+        };
       in
       {
+        packages.default = pokers;
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = pokers;
+        };
+
         devShells.default = pkgs.mkShell {
           buildInputs = [
             (pkgs.rust-bin.stable.latest.default.override {
